94edd2955cb7580a6a60bfcc465fd543e6625afd
Nick Ippoliti <yoshi445511@gmail.com>
2010-08-27 17:18:30 -0400

    AutoUpdate for music script

--- musicScpt/musicUpdate.sh
+++ musicScpt/musicUpdate.sh
@@ -1,1 +1,1 @@
-osascript "music.scpt" && cd ../ && git add * && git commit -m "AutoUpdate for music script" && git push origin master
+osascript "music.scpt" > music.txt && cd ../ && git add * && git commit -m "AutoUpdate for music script" && git push origin master
